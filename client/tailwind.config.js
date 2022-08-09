3d4885fcdf5f4abde2098d991a091d39ec0c9f2e
christhopermarcelino <christhopermarcelino@gmail.com>
2022-08-09 11:46:31 +0700

    [F] feat: add base style

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#03428b",
+        secondary: "#f8c300",
+      },
+    },
   },
   plugins: [],
 };
