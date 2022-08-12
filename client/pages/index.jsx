3245ff1f4681d6862a15ffba5b2c4e2a4fc2e1ce
christhopermarcelino <christhopermarcelino@gmail.com>
2022-08-12 11:54:23 +0700

    [F] feat: add activities table in dashboard

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,23 +1,136 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
 import Dashboard from "@/components/Dashboard";
 import { useAuthState } from "@/contexts/AuthContext";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { bearerToken } from "@/libs/helpers";
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+  columnHelper.accessor("time", {
+    cell: (info) => info.getValue(),
+    header: "Waktu",
+  }),
+  columnHelper.accessor("name", {
+    cell: (info) => info.getValue(),
+    header: "Nama",
+  }),
+  columnHelper.accessor("nim", {
+    cell: (info) => info.getValue(),
+    header: "NIM",
+  }),
+  columnHelper.accessor("code", {
+    cell: (info) => info.getValue(),
+    header: "Kode kartu",
+  }),
+];
 
 export default function Home() {
   const { user } = useAuthState();
   const router = useRouter();
+  const [tableData, setTableData] = useState([]);
+
+  axios.defaults.baseURL = "https://rfid-card-identifier.herokuapp.com/api";
 
   useEffect(() => {
     if (process.env.NODE_ENV === "production" && user === null) {
       router.replace("/signin");
     }
+
+    axios
+      .get("/activities", {}, { headers: { ...bearerToken() } })
+      .then((res) => setTableData(res.data.data))
+      .catch((err) => alert(err));
   }, []);
+
+  const [data, setData] = useState(() => [...tableData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
   return (
     <div>
       <Dashboard title='Data Statistik'>
         <main>
-          <h1>RFIF Card Identifier</h1>
+          <div>
+            <h2 className='mb-4'>Tabel Aktivitas</h2>
+            <div className='flex flex-col'>
+              <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                <div className='inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8'>
+                  <div className='overflow-hidden border-b border-gray-200 shadow sm:rounded-lg'>
+                    <table className='min-w-full divide-y divide-gray-200'>
+                      <thead className='bg-gray-50'>
+                        {table.getHeaderGroups().map((headerGroup, idx) => (
+                          <tr key={headerGroup.id}>
+                            <th
+                              key='no'
+                              scope='col'
+                              className='px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase'
+                            >
+                              No
+                            </th>
+                            {headerGroup.headers.map((header) => (
+                              <th
+                                key={header.id}
+                                scope='col'
+                                className='px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase'
+                              >
+                                {header.isPlaceholder
+                                  ? null
+                                  : flexRender(
+                                      header.column.columnDef.header,
+                                      header.getContext()
+                                    )}
+                              </th>
+                            ))}
+                          </tr>
+                        ))}
+                      </thead>
+                      <tbody>
+                        {table.getRowModel().rows.map((row, idx) => (
+                          <tr
+                            key={row.id}
+                            className={
+                              idx % 2 === 0 ? "bg-white" : "bg-gray-50"
+                            }
+                          >
+                            <td
+                              key='no'
+                              className='px-6 py-4 text-sm text-gray-500 whitespace-nowrap'
+                            >
+                              {idx}
+                            </td>
+                            {row.getVisibleCells().map((cell) => (
+                              <td
+                                key={cell.id}
+                                className='px-6 py-4 text-sm text-gray-500 whitespace-nowrap'
+                              >
+                                {flexRender(
+                                  cell.column.columnDef.cell,
+                                  cell.getContext()
+                                )}
+                              </td>
+                            ))}
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </main>
       </Dashboard>
     </div>
