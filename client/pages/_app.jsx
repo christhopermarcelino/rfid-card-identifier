a833042cc6da6dd622876bd0d8ad6ac79945b3d6
christhopermarcelino <christhopermarcelino@gmail.com>
2022-08-12 10:42:45 +0700

    [F] fix: render error due to router

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,31 +1,13 @@
 import "@/styles/globals.css";
-// import { useRouter } from "next/router";
 
 import { AuthProvider } from "@/contexts/AuthContext";
 
-const publicAccess = ["/", "/signin"];
-
 function MyApp({ Component, pageProps }) {
-  // const router = useRouter();
-
-  // if (
-  //   process.env.NODE_ENV === "production" &&
-  //   !publicAccess.includes(router.pathname)
-  // ) {
-  //   router.push("/");
-  // }
-
   return (
     <AuthProvider>
       <Component {...pageProps} />
     </AuthProvider>
   );
-
-  // return (
-  //   <AuthProvider>
-  //     <Layout Component={Component} pageProps={pageProps} />
-  //   </AuthProvider>
-  // );
 }
 
 export default MyApp;
