09115719c6feefb721dab60407b31bffe9e33755
christhopermarcelino <christhopermarcelino@gmail.com>
2022-08-18 04:43:19 +0700

    [B] feat: add devtoken authorization on helpers

--- client/libs/helpers.jsx
+++ client/libs/helpers.jsx
@@ -6,9 +6,8 @@ export function bearerToken(contentType) {
   const token = localStorage.getItem("token");
   const headers = {};
 
-  if (token) {
-    headers["Authorization"] = `Bearer ${token}`;
-  }
+  if (token) headers["Authorization"] = `Bearer ${token}`;
+  else headers["devtoken"] = process.env.DEV_TOKEN;
 
   return headers;
 }
