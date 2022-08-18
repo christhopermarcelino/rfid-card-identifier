36130c6cfd9ed0bf38a8e6d9a8824b716d46c18a
christhopermarcelino <christhopermarcelino@gmail.com>
2022-08-19 00:39:36 +0700

    [B] style: remove unused function

--- server/routers/CardRouter.js
+++ server/routers/CardRouter.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const {
   updateTemporaryCard,
   getTemporaryCard,
-  insertNewCard,
   pairCardWithUser,
 } = require("../controllers/CardController");
 
 router.get("/update-temp", updateTemporaryCard);
 router.get("/get-temp", getTemporaryCard);
-router.post("/insert", insertNewCard);
 router.post("/pair", pairCardWithUser);
 
 module.exports = router;
